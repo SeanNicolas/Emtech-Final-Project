3fe7ebac22f3437059aaf20e9314eae762edc14a
Sean Julian Nicolas <116535699+SeanNicolas@users.noreply.github.com>
2024-05-17 00:13:58 +0800

    Add files via upload

---
+++ 1_👀_Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title= "Retinopathy",
+    page_icon = "👁️"
+)
+
+st.title("What is Retinopathy?")
+st.sidebar.success("Select a Page")
+
+st.text("sinfjbwefbwfnjwjefnwef")
