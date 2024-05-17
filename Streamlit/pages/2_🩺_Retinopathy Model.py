23306dd72606db319c0107ad25fdbfe4f51e715b
Christian Jay L. Cuevas <143265700+ChristianJayCuevas@users.noreply.github.com>
2024-05-17 11:46:43 +0800

    Update 2_🩺_Retinopathy Model.py

--- Streamlit/pages/2_🩺_Retinopathy Model.py
+++ Streamlit/pages/2_🩺_Retinopathy Model.py
@@ -25,8 +25,7 @@ def import_and_predict(image_data,model):
     size = (50, 50)
     image = ImageOps.fit(image_data, size, Image.Resampling.LANCZOS)
     img = np.asarray(image)
-    img_reshape = img[np.newaxis, ...]
-    prediction = model.predict(img_reshape)
+    prediction = model.predict(img)
     return prediction
 if file is None:
     st.text("Please upload an image file")
