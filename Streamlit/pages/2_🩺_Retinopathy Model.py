1280c5da28a9fbba77c8228b4fa1c2ec60f44e7c
Christian Jay L. Cuevas <143265700+ChristianJayCuevas@users.noreply.github.com>
2024-05-17 09:25:30 +0800

    Update 2_🩺_Retinopathy Model.py

--- Streamlit/pages/2_🩺_Retinopathy Model.py
+++ Streamlit/pages/2_🩺_Retinopathy Model.py
@@ -11,7 +11,7 @@ st.sidebar.success("Select a Page")
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model('model3(1).hdf5')
+  model=tf.keras.models.load_model('Streamlit/model3 (1).h5')
   return model
 model=load_model()
 st.write("""
